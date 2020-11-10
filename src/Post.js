2a213a7aca59800ee0391c2c5beb15012ee2f69f
RukshanNuwan <39560159+RukshanNuwan@users.noreply.github.com>
2020-11-10 13:57:36 +0530

    Create class component

---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// Create Class
+class Post extends Component {
+    // render(){} - Important Function in Class Components
+    render() {
+        return (
+            <div>
+                <h2>Hello</h2>
+            </div>
+        );
+    }
+}
+
+export default Post;
